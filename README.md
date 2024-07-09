5d3b3876695408b2096c587b9f6da366a9752e46
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-07-09 15:18:16 -0400

    feat: Cache all the kernels. No Akmods Bootstrap
    fix: headers only exist for fsync
    
    fix: typo for download
    
    fix: no kernel-surface-uki

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# fsync
+# Kernel Cache
 
 [![Cache Fsync](https://github.com/ublue-os/fsync/actions/workflows/reusable-build.yml/badge.svg)](https://github.com/ublue-os/fsync/actions/workflows/reusable-build.yml)
 
-A caching layer for the fsync kernel from sentry/kernel-fsync
+A caching layer for the different kernels used by the Universal Blue Project
--- fetch.sh
+++ fetch.sh
@@ -2,23 +2,70 @@
 
 set -eoux pipefail
 
-kernel_version="${KERNEL_VERSION}".fsync.fc"${FEDORA_VERSION}".x86_64
+kernel_version="${KERNEL_VERSION}"
+kernel_flavor="${KERNEL_FLAVOR}"
 
-curl -LsSf -o /etc/yum.repos.d/_copr_sentry-kernel-ba.repo \
-    https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-"$(rpm -E %fedora)"/sentry-kernel-fsync-fedora-"$(rpm -E %fedora)".repo
+dnf install -y dnf-plugins-core
 
-rpm-ostree install -y dnf dnf-plugins-core
+case "$kernel_flavor" in
+    "asus")
+        dnf copr enable -y lukenukem/asus-kernel
+        ;;
+    "fsync")
+        dnf copr enable -y sentry/kernel-fsync
+        ;;
+    "surface")
+        dnf config-manager --add-repo=https://pkg.surfacelinux.com/fedora/linux-surface.repo
+        ;;
+    "coreos-stable")
+        ;;
+    "coreos-testing")
+        ;;
+    "main")
+        ;;
+    *)
+        echo "unexpected kernel_flavor ${kernel_flavor} for query"
+        ;;
+esac
 
-dnf download -y \
-    kernel-"${kernel_version}" \
-    kernel-core-"${kernel_version}" \
-    kernel-devel-matched-"${kernel_version}" \
-    kernel-modules-"${kernel_version}" \
-    kernel-modules-core-"${kernel_version}" \
-    kernel-modules-extra-"${kernel_version}" \
-    kernel-headers-"${kernel_version}" \
-    kernel-devel-"${kernel_version}" \
-    kernel-uki-virt-"${kernel_version}"
+if [[ "${kernel_flavor}" =~ asus|fsync ]]; then
+    dnf download -y \
+        kernel-"${kernel_version}" \
+        kernel-core-"${kernel_version}" \
+        kernel-modules-"${kernel_version}" \
+        kernel-modules-core-"${kernel_version}" \
+        kernel-modules-extra-"${kernel_version}" \
+        kernel-devel-"${kernel_version}" \
+        kernel-devel-matched-"${kernel_version}" \
+        kernel-uki-virt-"${kernel_version}"
+elif [[ "${kernel_flavor}" == "surface" ]]; then
+    dnf download -y \
+        kernel-surface-"${kernel_version}" \
+        kernel-surface-core-"${kernel_version}" \
+        kernel-surface-modules-"${kernel_version}" \
+        kernel-surface-modules-core-"${kernel_version}" \
+        kernel-surface-modules-extra-"${kernel_version}" \
+        kernel-surface-devel-"${kernel_version}" \
+        kernel-surface-devel-matched-"${kernel_version}"
+else
+    KERNEL_MAJOR_MINOR_PATCH=$(echo "$kernel_version" | cut -d '-' -f 1)
+    KERNEL_RELEASE="$(echo "$kernel_version" | cut -d - -f 2 | cut -d . -f 1).$(echo "$kernel_version" | cut -d - -f 2 | cut -d . -f 2)"
+    ARCH=$(uname -m)
+    dnf download -y \
+        https://kojipkgs.fedoraproject.org//packages/kernel/"$KERNEL_MAJOR_MINOR_PATCH"/"$KERNEL_RELEASE"/"$ARCH"/kernel-"$kernel_version".rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/"$KERNEL_MAJOR_MINOR_PATCH"/"$KERNEL_RELEASE"/"$ARCH"/kernel-core-"$kernel_version".rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/"$KERNEL_MAJOR_MINOR_PATCH"/"$KERNEL_RELEASE"/"$ARCH"/kernel-modules-"$kernel_version".rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/"$KERNEL_MAJOR_MINOR_PATCH"/"$KERNEL_RELEASE"/"$ARCH"/kernel-modules-core-"$kernel_version".rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/"$KERNEL_MAJOR_MINOR_PATCH"/"$KERNEL_RELEASE"/"$ARCH"/kernel-modules-extra-"$kernel_version".rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/"$KERNEL_MAJOR_MINOR_PATCH"/"$KERNEL_RELEASE"/"$ARCH"/kernel-devel-"$kernel_version".rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/"$KERNEL_MAJOR_MINOR_PATCH"/"$KERNEL_RELEASE"/"$ARCH"/kernel-devel-matched-"$kernel_version".rpm \
+        https://kojipkgs.fedoraproject.org//packages/kernel/"$KERNEL_MAJOR_MINOR_PATCH"/"$KERNEL_RELEASE"/"$ARCH"/kernel-uki-virt-"$kernel_version".rpm
+fi
+
+if [[ "${kernel_flavor}" =~ fsync ]]; then
+    dnf download -y \
+        kernel-headers-"${kernel_version}"
+fi
 
 mkdir -p /tmp/rpms
 

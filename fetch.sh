390dd59a09012154a1160c3cf3a15fd5344821a3
Antheas Kapenekakis <5252246+antheas@users.noreply.github.com>
2024-10-18 18:44:34 +0200

    feat: add kernel-bazzite to cache (#29)
    * feat: add kernel-bazzite to cache
    
    * fix: Pass BUILD_TAG to Containerfile
    
    * fix: add bazzite tag to fetch
    
    * fix: add missing kernel-core module
    
    ---------
    
    Co-authored-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- fetch.sh
+++ fetch.sh
@@ -4,6 +4,7 @@ set -eoux pipefail
 
 kernel_version="${KERNEL_VERSION}"
 kernel_flavor="${KERNEL_FLAVOR}"
+build_tag="${KERNEL_BUILD_TAG:-latest}"
 
 # CoreOS pool repo
 # curl -LsSf -o /etc/yum.repos.d/fedora-coreos-pool.repo \
@@ -27,6 +28,8 @@ case "$kernel_flavor" in
     "coreos-stable")
         ;;
     "coreos-testing")
+        ;;
+    "bazzite")
         ;;
     "main")
         ;;
@@ -57,7 +60,17 @@ elif [[ "${kernel_flavor}" == "surface" ]]; then
         iptsd \
         libwacom-surface \
         libwacom-surface-data
-
+elif [[ "${kernel_flavor}" == "bazzite" ]]; then
+    # Using curl for bazzite release
+    curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-"$kernel_version".rpm
+    curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-core-"$kernel_version".rpm
+    curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-modules-"$kernel_version".rpm
+    curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-modules-core-"$kernel_version".rpm
+    curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-modules-extra-"$kernel_version".rpm
+    curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-devel-"$kernel_version".rpm
+    curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-devel-matched-"$kernel_version".rpm
+    curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-uki-virt-"$kernel_version".rpm
+    # curl -LO https://github.com/hhd-dev/kernel-bazzite/releases/download/"$build_tag"/kernel-uki-virt-addons-"$kernel_version".rpm
 else
     KERNEL_MAJOR_MINOR_PATCH=$(echo "$kernel_version" | cut -d '-' -f 1)
     KERNEL_RELEASE="$(echo "$kernel_version" | cut -d - -f 2 | cut -d . -f 1).$(echo "$kernel_version" | cut -d - -f 2 | cut -d . -f 2)"
@@ -107,6 +120,13 @@ elif [[ "${kernel_flavor}" =~ surface ]]; then
         /kernel-surface-modules-core-"$kernel_version".rpm \
         /kernel-surface-modules-extra-"$kernel_version".rpm \
         kernel-surface-core-"${kernel_version}"
+elif [[ "${kernel_flavor}" == "bazzite" ]]; then
+    dnf install -y \
+        /kernel-"$kernel_version".rpm \
+        /kernel-core-"$kernel_version".rpm \
+        /kernel-modules-"$kernel_version".rpm \
+        /kernel-modules-core-"$kernel_version".rpm \
+        /kernel-modules-extra-"$kernel_version".rpm
 else
     dnf install -y \
         /kernel-"$kernel_version".rpm \
